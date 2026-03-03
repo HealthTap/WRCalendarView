40ae99cfa03966efedb65880d1021e9820062424
uditkakkad <udit.kakkad@healthtap.com>
2026-03-03 18:03:00 +0530

    SUNP-21032 Bug fix.

--- Sources/WRCalendarView/Views/WRRowHeader.swift
+++ Sources/WRCalendarView/Views/WRRowHeader.swift
@@ -17,7 +17,7 @@ class WRRowHeader: UICollectionReusableView {
     override func awakeFromNib() {
         super.awakeFromNib()
         dateFormatter.dateFormat = "h:mm a"
-        layer.zPosition = 2
+        layer.zPosition = 3
     }
 
     var date: Date? {
--- Sources/WRCalendarView/Views/WRRowHeaderBackground.swift
+++ Sources/WRCalendarView/Views/WRRowHeaderBackground.swift
@@ -11,6 +11,7 @@ class WRRowHeaderBackground: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.white
+        layer.zPosition = 2
 //        let gridThickness: CGFloat = UIScreen.main.scale == 2 ? 0.5 : 1.0
 //        addBorder(edge: .right, color: ColorUtil.uiColorFromHex("dadada"), borderWidth: gridThickness)
     }
